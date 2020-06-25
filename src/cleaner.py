03ced863ef22b27f57578638b9ec1580412ad9da
b-weintraub <61023782+b-weintraub@users.noreply.github.com>
2020-06-25 14:53:50 -0700

    add data cleaner

---
+++ src/cleaner.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def cleaner(df):
+    df['name'] = df['tFname'] + ' ' + df['tLname']
+    df.drop(['tFname','tMiddlename','tLname','Unnamed: 0','contentType','categoryType'], axis=1, inplace=True)
+    df.dropna(inplace=True)
+    return df
+
+
+def main():
+    
+    df = pd.read_csv('data/PC_data.csv')
+    clean_data = cleaner(df)
+    
+    clean_data.to_csv('data/clean_data.csv')
+
+
+if __name__ == '__main__':
+    main()
